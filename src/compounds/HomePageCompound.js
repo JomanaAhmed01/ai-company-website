41c898e8c6f0ac791fc531e7f3e1de4a434cc63c
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-06 23:10:58 +0200

    Creating HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,10 +2,24 @@ import './App.css';
 
 function HomePageCompound() {
   return (
-    <>
-      
-    </>
+    <Wrapper>
+      <HeaderWrapper>
+        
+      </HeaderWrapper>
+    </Wrapper>
   );
 }
+
+export const Wrapper = styled.div`
+  
+`
+
+export const HeaderWrapper = styled.div`
+  background-image: url('./img/bg.png');
+  width: 1349px;
+  height: 1081px;
+  object-fit: cover;
+  object-position: 50% 100%;
+`
 
 export default HomePageCompound;
