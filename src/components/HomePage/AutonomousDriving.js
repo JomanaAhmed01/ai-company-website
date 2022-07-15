c3a4f20fe16027f84037b1a0f577102a18b3dc24
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-15 14:10:03 +0200

    Creating AutonomousDriving.js file

---
+++ src/components/HomePage/AutonomousDriving.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components"
+
+function AutonomousDriving() {
+    return (
+        <Wrapper>
+            <TextWrapper>
+              <Header>AUTONOMOUS DRIVING</Header>
+            </TextWrapper>
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export const TextWrapper = styled.div`
+  
+`
+
+export const Header = styled.div`
+  
+`
+
+export default AutonomousDriving;
