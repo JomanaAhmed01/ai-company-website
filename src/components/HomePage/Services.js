bb1f2c36b665ebf303feefb0a8be4ffde983c3b4
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-15 14:04:59 +0200

    Creating AI-company project's Services section

--- src/components/HomePage/Services.js
+++ src/components/HomePage/Services.js
@@ -4,13 +4,57 @@ import styled from "styled-components"
 function Services() {
     return (
         <Wrapper>
-
+            <TextWrapper>
+                <HeaderText>SERVICES</HeaderText>
+                <Header>We Deliver Exceptional Products and Services Around the World</Header>
+            </TextWrapper>
         </Wrapper>
     );
 }
 
 export const Wrapper = styled.div`
+  margin-top: 150px;
+  padding-left: 50px;
+
+  @media screen and (max-width: 510px) {
+    display: flex;
+    justify-content: center;
+    padding-left: 0px;
+  }
+`
+
+export const TextWrapper = styled.div`
+  border-left: 1px solid rgb(180,180,180);
+  padding-left: 50px;
+  margin-left: 50px;
+`
+
+export const HeaderText = styled.p`
+  color: #000000;
+  font-size: 14px;
+  font-family: 'Raleway', sans-serif;
+  letter-spacing: 0.08em;
+`
+
+export const Header = styled.p`
+  color: #000000;
+  font-size: 24px;
+  font-family: 'Raleway', sans-serif;
+  line-height: 1.8em;
+  letter-spacing: 0.08em;
+  width: 320px;
+  border-left: 3px solid #000000;
+  margin-left: -50px;
+  padding-left: 50px;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 20px;
+  }
   
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+    width: 90%;
+  }
 `
 
 export default Services;
