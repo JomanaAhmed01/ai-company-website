6368dbdfb0a2953ec62589426192d638ddedfeb0
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-15 22:05:49 +0200

    Creating AI-company project's Why autono section

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -7,6 +7,7 @@ import Services from '../components/HomePage/Services'
 import AutonomousDriving from '../components/HomePage/AutonomousDriving'
 import RealTimeInfo from '../components/HomePage/RealTimeInfo'
 import PercepEnabled from '../components/HomePage/PercepEnabled'
+import WhyAutono from '../components/HomePage/WhyAutono'
 
 function HomePageCompound() {
   return (
@@ -21,6 +22,7 @@ function HomePageCompound() {
       <AutonomousDriving />
       <RealTimeInfo />
       <PercepEnabled />
+      <WhyAutono />
     </Wrapper>
   );
 }
