3f9ee98d308c08472a88cc4928f2c3fa27bfb39d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 21:31:01 +0200

    Adjusting the code

--- src/components/Technology/TheAutonoCarText.js
+++ src/components/Technology/TheAutonoCarText.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import styled from "styled-components"
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 
 function TheAutonoCarText() {
-    return (
-        <Wrapper>
-            <TextOneWrapper>
-                <HeaderOne>HUMAN-CENTERED DESIGN</HeaderOne>
-                <TextOne>I'm a paragraph.
-                    Click here to add your own text and edit me.
-                    It’s easy.
-                    Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                    I’m a great place for you to tell a story and let your users know a little more about you.
-                </TextOne>
-            </TextOneWrapper>
+  return (
+    <Wrapper>
+      <TextOneWrapper>
+        <HeaderOne>HUMAN-CENTERED DESIGN</HeaderOne>
+        <TextOne>I'm a paragraph.
+          Click here to add your own text and edit me.
+          It’s easy.
+          Just click “Edit Text” or double click me to add your own content and make changes to the font.
+          I’m a great place for you to tell a story and let your users know a little more about you.
+        </TextOne>
+      </TextOneWrapper>
 
-            <TextTwoWrapper>
-                <HeaderTwo>​LOW EMISSIONS & EFFICIENCY</HeaderTwo>
-                <TextTwo>I'm a paragraph.
-                    Click here to add your own text and edit me.
-                    It’s easy.
-                    Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                    I’m a great place for you to tell a story and let your users know a little more about you.</TextTwo>
-            </TextTwoWrapper>
-        </Wrapper>
-    );
+      <TextTwoWrapper>
+        <HeaderTwo>​LOW EMISSIONS & EFFICIENCY</HeaderTwo>
+        <TextTwo>I'm a paragraph.
+          Click here to add your own text and edit me.
+          It’s easy.
+          Just click “Edit Text” or double click me to add your own content and make changes to the font.
+          I’m a great place for you to tell a story and let your users know a little more about you.</TextTwo>
+      </TextTwoWrapper>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
