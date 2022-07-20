3d670231098a725bd9c3d0864b77bc11a8af8dd2
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 20:17:36 +0200

    Adjusting the code

--- src/components/Technology/Header.js
+++ src/components/Technology/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from "styled-components"
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function Header() {
-    return (
-        <Wrapper>
-            <TheHeader>PERCEPTION ENABLED TECHNOLOGY</TheHeader>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <TheHeader>PERCEPTION ENABLED TECHNOLOGY</TheHeader>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
