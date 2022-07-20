27bff9529931f0e2619e9167b5e0857ab216fcd1
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 20:07:24 +0200

    Hiding the HomePageCompound.js file link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePageCompound from './compounds/HomePageCompound'
+// import HomePageCompound from './compounds/HomePageCompound'
 import TechnologyPageCompound from './compounds/TechnologyPageCompound';
 
 function App() {
