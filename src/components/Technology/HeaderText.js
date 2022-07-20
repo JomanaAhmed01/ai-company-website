a7997136efe9ef616c2385fca5df8a5b41957b5c
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 20:05:29 +0200

    Adding margi-bottom to the Header text section

--- src/components/Technology/HeaderText.js
+++ src/components/Technology/HeaderText.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styled from "styled-components"
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function HeaderText() {
-    return (
-        <Wrapper>
-            <Text>Click here to edit this paragraph and add your own content.
-                This is a great place to expand on the title and any relevant details or information.
-            </Text>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Text>Click here to edit this paragraph and add your own content.
+        This is a great place to expand on the title and any relevant details or information.
+      </Text>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
-  /* margin-top: 70px; */
+  margin-bottom: 100px;
 
   @media screen and (max-width: 768px) {
     margin-top: -70px;
