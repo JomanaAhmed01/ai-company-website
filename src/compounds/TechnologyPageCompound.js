9d6b3e88434c7a7e3eaae9a231b90010bc930286
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 13:33:52 +0200

    Defining Header.js file in the TechnologyPageCompound.js file

--- src/compounds/TechnologyPageCompound.js
+++ src/compounds/TechnologyPageCompound.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from "styled-components"
-import Navbar from '../components/Technology/Navbar';
+import Navbar from '../components/Technology/Navbar'
+import Header from '../components/Technology/Header'
 
 function TechnologyPageCompound() {
   return (
     <Wrapper>
       <HeaderWrapper>
         <Navbar />
+        <Header />
       </HeaderWrapper>
     </Wrapper>
   );
@@ -17,7 +19,19 @@ export const Wrapper = styled.div`
 `
 
 export const HeaderWrapper = styled.div`
+  background-image: url('./img/car-4.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
   height: 1060px;
+
+  @media screen and (max-width: 650px) {
+    height: 850px;
+  }
+
+  @media screen and (max-width: 505px) {
+    height: 700px;
+  }
 `
 
 export default TechnologyPageCompound;
