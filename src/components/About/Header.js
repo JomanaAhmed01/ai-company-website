1ccf475c77d0f8cc4614939db7f9139303f997a6
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-20 22:46:43 +0200

    Adjusting the code

--- src/components/About/Header.js
+++ src/components/About/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styled from "styled-components"
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function Header() {
-    return (
-        <Wrapper>
-            <TheHeader>OUR JOURNEY</TheHeader>
-            <HeaderText>I'm a paragraph.
-                Click here to add your own text and edit me.
-                It’s easy.
-                Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </HeaderText>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <TheHeader>OUR JOURNEY</TheHeader>
+      <HeaderText>I'm a paragraph.
+        Click here to add your own text and edit me.
+        It’s easy.
+        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+        I’m a great place for you to tell a story and let your users know a little more about you.
+      </HeaderText>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
