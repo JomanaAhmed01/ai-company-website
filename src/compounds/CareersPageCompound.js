cefc1e0a2372e16dfbdf233b22db9cda66a50e27
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-21 14:40:09 +0200

    Defining Location.js file in the CareersPageCompound.js file

--- src/compounds/CareersPageCompound.js
+++ src/compounds/CareersPageCompound.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/Careers/Navbar'
 import Header from '../components/Careers/Header'
+import Location from '../components/Careers/Location'
 
 function CareersPageCompound() {
   return (
@@ -10,6 +11,8 @@ function CareersPageCompound() {
         <Navbar />
         <Header />
       </HeaderWrapper>
+
+      <Location />
     </Wrapper>
   );
 }
