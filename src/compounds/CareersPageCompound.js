5bc7138011d8e94628ab1bb870f2198d26236d14
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-21 14:21:39 +0200

    Defining Header.js file in the CareersPageCompound.js

--- src/compounds/CareersPageCompound.js
+++ src/compounds/CareersPageCompound.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/Careers/Navbar'
+import Header from '../components/Careers/Header'
 
 function CareersPageCompound() {
-    return (
-        <Wrapper>
-          <HeaderWrapper>
-            <Navbar />
-          </HeaderWrapper>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <HeaderWrapper>
+        <Navbar />
+        <Header />
+      </HeaderWrapper>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
