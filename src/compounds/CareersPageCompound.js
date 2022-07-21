4385e5a930b479182810b3800876a83d4ed1a073
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-21 14:12:09 +0200

    Defining Navbar.js file in the CareersPageCompound.js file

--- src/compounds/CareersPageCompound.js
+++ src/compounds/CareersPageCompound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
+import Navbar from '../components/Careers/Navbar'
 
 function CareersPageCompound() {
     return (
         <Wrapper>
-
+          <HeaderWrapper>
+            <Navbar />
+          </HeaderWrapper>
         </Wrapper>
     );
 }
