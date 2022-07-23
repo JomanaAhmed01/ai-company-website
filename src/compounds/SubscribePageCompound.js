b5c8a72e4ca08b2ebd51e27f65477b64f956c558
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-23 14:32:08 +0200

    Creating SubscribePageCompound.js file

---
+++ src/compounds/SubscribePageCompound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components"
+
+function SubscribePageCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export const HeaderWrapper = styled.div`
+  
+`
+
+export default SubscribePageCompound;
