b92c856613944402aba06ebfd24984610b484089
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-23 14:37:49 +0200

    Defining Navbar.js file in the SubscribePageCompound.js file

--- src/compounds/SubscribePageCompound.js
+++ src/compounds/SubscribePageCompound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components"
+import Navbar from '../components/Subscribe/Navbar'
 
 function SubscribePageCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
