67a74ca13484b454d94338196c90e90dc5079d7c
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-17 17:50:05 +0200

    Adjusting the code

--- src/components/HomePage/RealTimeInfo.js
+++ src/components/HomePage/RealTimeInfo.js
@@ -2,31 +2,31 @@ import React from 'react';
 import styled from "styled-components"
 import { ArrowRight } from '@styled-icons/bootstrap/ArrowRight'
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function RealTimeInfo() {
-    return (
-        <Wrapper>
-            <ImageWrapper>
-                <Image src='./img/speed-limit.png' />
-            </ImageWrapper>
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Image src='./img/speed-limit.png' />
+      </ImageWrapper>
 
-            <TextWrapper>
-                <Header>REAL-TIME INFORMATION</Header>
-                <Text>I'm a paragraph.
-                    Click here to add your own text and edit me.
-                    It’s easy.
-                    Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                    I’m a great place for you to tell a story and let your users know a little more about you.
-                </Text>
-                <ButtonWrapper>
-                    <Button>Read More</Button>
-                    <Arrow />
-                </ButtonWrapper>
-            </TextWrapper>
-        </Wrapper>
-    );
+      <TextWrapper>
+        <Header>REAL-TIME INFORMATION</Header>
+        <Text>I'm a paragraph.
+          Click here to add your own text and edit me.
+          It’s easy.
+          Just click “Edit Text” or double click me to add your own content and make changes to the font.
+          I’m a great place for you to tell a story and let your users know a little more about you.
+        </Text>
+        <ButtonWrapper>
+          <Button>Read More</Button>
+          <Arrow />
+        </ButtonWrapper>
+      </TextWrapper>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
