f92bb58cb2321cd732148e7e81311882fee0701d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-09-17 13:23:53 +0200

    Adding import link for the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import HomePageCompound from './compounds/HomePageCompound'
 // import TechnologyPageCompound from './compounds/TechnologyPageCompound'
 // import AboutPageCompound from './compounds/AboutPageCompound'
