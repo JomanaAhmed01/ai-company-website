9be6b139cc0596fef210259110009e761b84ff7d
Jomana ahmed <ojy.ahmed@outlook.com>
2025-07-30 20:22:37 +0200

    Aligning the code in a neat way

--- src/components/HomePage/Vision.js
+++ src/components/HomePage/Vision.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
+;<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
-
 
 function Vision() {
   return (
@@ -11,19 +11,19 @@ function Vision() {
       <TextWrapper>
         <HeaderText>VISION</HeaderText>
         <Header>We’re Changing the Way the World Thinks About Cars</Header>
-        <Text>I'm a paragraph.
-          Click here to add your own text and edit me.
-          It’s easy.
-          Just click “Edit Text” or double click me to add your own content and make changes to the font.
-          I’m a great place for you to tell a story and let your users know a little more about you.
+        <Text>
+          I'm a paragraph. Click here to add your own text and edit me. It’s
+          easy. Just click “Edit Text” or double click me to add your own
+          content and make changes to the font. I’m a great place for you to
+          tell a story and let your users know a little more about you.
         </Text>
       </TextWrapper>
 
       <ImageWrapper>
-        <Image src='./img/car-1.png' />
+        <Image src="./img/car-1.png" />
       </ImageWrapper>
     </Wrapper>
-  );
+  )
 }
 
 export const Wrapper = styled.div`
@@ -42,7 +42,7 @@ export const Wrapper = styled.div`
 `
 
 export const TextWrapper = styled.div`
-  border-left: 1px solid rgb(180,180,180);
+  border-left: 1px solid rgb(180, 180, 180);
   padding-left: 50px;
   margin-left: 50px;
   height: 420px;
@@ -59,27 +59,27 @@ export const TextWrapper = styled.div`
 `
 
 export const HeaderText = styled.p`
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 14px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   letter-spacing: 0.08em;
 `
 
 export const Header = styled.p`
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 24px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   line-height: 1.8em;
   letter-spacing: 0.08em;
   width: 280px;
-  border-left: 3px solid #FFFFFF;
+  border-left: 3px solid #ffffff;
   margin-left: -50px;
   padding-left: 50px;
 
   @media screen and (max-width: 1024px) {
     font-size: 20px;
   }
-  
+
   @media screen and (max-width: 480px) {
     font-size: 18px;
     width: 90%;
@@ -87,9 +87,9 @@ export const Header = styled.p`
 `
 
 export const Text = styled.p`
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 16px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   letter-spacing: 0.08em;
   width: 335px;
   line-height: 30px;
@@ -104,9 +104,7 @@ export const Text = styled.p`
   }
 `
 
-export const ImageWrapper = styled.div`
-  
-`
+export const ImageWrapper = styled.div``
 
 export const Image = styled.img`
   width: 100%;
@@ -116,4 +114,4 @@ export const Image = styled.img`
   }
 `
 
-export default Vision;
+export default Vision
