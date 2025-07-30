656e40a7fc26f3692ac70d52abfceeba06e14652
Jomana ahmed <ojy.ahmed@outlook.com>
2025-07-30 12:25:51 +0200

    Changing the margin-top in the Main wrapper of the Autonomous driving section from 150px to 120px

--- src/components/HomePage/AutonomousDriving.js
+++ src/components/HomePage/AutonomousDriving.js
@@ -37,7 +37,7 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-top: 150px;
+  margin-top: 120px;
 
   @media screen and (max-width: 1024px) {
     flex-direction: column;
