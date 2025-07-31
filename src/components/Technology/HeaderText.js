6e1a1db902b4efce1b1f95b6b4e5a3a6b055bc27
Jomana ahmed <ojy.ahmed@outlook.com>
2025-07-31 17:42:44 +0200

    Aligning the code in a neat way

--- src/components/Technology/HeaderText.js
+++ src/components/Technology/HeaderText.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+;<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function HeaderText() {
   return (
     <Wrapper>
-      <Text>Click here to edit this paragraph and add your own content.
-        This is a great place to expand on the title and any relevant details or information.
+      <Text>
+        Click here to edit this paragraph and add your own content. This is a
+        great place to expand on the title and any relevant details or
+        information.
       </Text>
     </Wrapper>
-  );
+  )
 }
 
 export const Wrapper = styled.div`
@@ -25,7 +28,7 @@ export const Wrapper = styled.div`
 export const Text = styled.p`
   color: #000000;
   font-size: 24px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   letter-spacing: 0.05em;
   line-height: 40px;
   width: 730px;
@@ -43,4 +46,4 @@ export const Text = styled.p`
   }
 `
 
-export default HeaderText;
+export default HeaderText
