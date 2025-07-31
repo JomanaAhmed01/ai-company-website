4d6c8331d59fa46bf00c2a969797547bf2623412
Jomana ahmed <ojy.ahmed@outlook.com>
2025-07-31 17:54:33 +0200

    Aligning the code in a neat way

--- src/components/Technology/TheAutonoCar.js
+++ src/components/Technology/TheAutonoCar.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
+;<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
-
 
 function TheAutonoCar() {
   return (
     <Wrapper>
       <TextWrapper>
         <HeaderText>THE AUTONO CAR</HeaderText>
-        <Header>Using groundbreaking perception-enabled sensor technology,
-          the Autono car is a fully driverless vehicle engineered to the highest degree of precision and safety.
-          It’s autonomous driving reimagined.
+        <Header>
+          Using groundbreaking perception-enabled sensor technology, the Autono
+          car is a fully driverless vehicle engineered to the highest degree of
+          precision and safety. It’s autonomous driving reimagined.
         </Header>
       </TextWrapper>
 
       <ImageWrapper>
-        <Image src='./img/car-5.png' />
+        <Image src="./img/car-5.png" />
       </ImageWrapper>
     </Wrapper>
-  );
+  )
 }
 
 export const Wrapper = styled.div`
@@ -40,7 +41,7 @@ export const Wrapper = styled.div`
 `
 
 export const TextWrapper = styled.div`
-  border-left: 1px solid rgb(180,180,180);
+  border-left: 1px solid rgb(180, 180, 180);
   padding-left: 50px;
   margin-left: 50px;
   height: 400px;
@@ -57,36 +58,34 @@ export const TextWrapper = styled.div`
 `
 
 export const HeaderText = styled.p`
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 14px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   letter-spacing: 0.08em;
 `
 
 export const Header = styled.p`
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 24px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   line-height: 1.8em;
   letter-spacing: 0.08em;
   width: 375px;
-  border-left: 3px solid #FFFFFF;
+  border-left: 3px solid #ffffff;
   margin-left: -50px;
   padding-left: 50px;
 
   @media screen and (max-width: 1024px) {
     font-size: 20px;
   }
-  
+
   @media screen and (max-width: 480px) {
     font-size: 18px;
     width: 90%;
   }
 `
 
-export const ImageWrapper = styled.div`
-  
-`
+export const ImageWrapper = styled.div``
 
 export const Image = styled.img`
   width: 100%;
@@ -96,4 +95,4 @@ export const Image = styled.img`
   }
 `
 
-export default TheAutonoCar;
+export default TheAutonoCar
