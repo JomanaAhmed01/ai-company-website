4feac787d2b9ec4e025c88255670786c00d7f5d5
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-02 18:04:21 +0200

    Adding the Banner that shows when clicking on the Menu icon on the mobile mode

--- src/compounds/TechnologyPageCompound.js
+++ src/compounds/TechnologyPageCompound.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { useHistory } from "react-router-dom"
 import Navbar from "../components/Technology/Navbar"
 import Header from "../components/Technology/Header"
 import HeaderText from "../components/Technology/HeaderText"
@@ -10,10 +11,42 @@ import TheAutonoDigitalText from "../components/Technology/TheAutonoDigitalText"
 import Footer from "../components/Technology/Footer"
 
 function TechnologyPageCompound() {
+  const [showBanner, setShowBanner] = React.useState(false)
+
+  const toggleBanner = () => {
+    setShowBanner(!showBanner)
+  }
+
+  const history = useHistory()
+
   return (
     <Wrapper>
+      {showBanner && (
+        <BannerWrapper>
+          <ItemsWrapper>
+            <CloseButton onClick={toggleBanner}>×</CloseButton>
+            <TechnologyOption
+              onClick={() => history.push("/TechnologyPageCompound")}
+            >
+              Technology
+            </TechnologyOption>
+            <AboutOption onClick={() => history.push("/AboutPageCompound")}>
+              About
+            </AboutOption>
+            <CareersOption onClick={() => history.push("/CareersPageCompound")}>
+              Careers
+            </CareersOption>
+            <SubscribeOption
+              onClick={() => history.push("/SubscribePageCompound")}
+            >
+              Subscribe
+            </SubscribeOption>
+          </ItemsWrapper>
+        </BannerWrapper>
+      )}
+
       <HeaderWrapper>
-        <Navbar />
+        <Navbar toggleBanner={toggleBanner} />
         <Header />
       </HeaderWrapper>
 
@@ -45,6 +78,70 @@ export const HeaderWrapper = styled.div`
   @media screen and (max-width: 505px) {
     height: 700px;
   }
+`
+
+export const BannerWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  width: 100vw;
+  height: 100vh;
+  font-family: "Anton", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
+
+export const ItemsWrapper = styled.p`
+  height: 400px;
+  background-color: #ffffff;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
+
+export const TechnologyOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const AboutOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const CareersOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const SubscribeOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #ffffff;
+  background-color: #000000;
+  padding: 10px 20px;
+  border-radius: 7px;
+`
+
+const CloseButton = styled.div`
+  position: absolute;
+  top: 30px;
+  right: 40px;
+  font-size: 40px;
+  color: white;
+  cursor: pointer;
 `
 
 export default TechnologyPageCompound
