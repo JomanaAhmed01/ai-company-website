7488f062b935e98f1f6e1d179d65c8844a20bbc2
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-02 18:33:34 +0200

    Fixing the Email input in the Subscribe page

--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+;<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
 </style>
 
 function Subscribe() {
@@ -20,7 +21,7 @@ function Subscribe() {
         </EmailWrapper>
       </SecondColumn>
     </Wrapper>
-  );
+  )
 }
 
 export const Wrapper = styled.div`
@@ -48,35 +49,25 @@ export const SecondColumn = styled.div`
 export const Header = styled.p`
   color: #000000;
   font-size: 18px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   letter-spacing: 0.04em;
   margin-bottom: 50px;
-
-  @media screen and (max-width: 930px) {
-    text-align: center;
-  }
 `
 
 export const Text = styled.p`
   color: #000000;
   font-size: 16px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   letter-spacing: 0.04em;
   margin-bottom: 50px;
-  
-  @media screen and (max-width: 930px) {
-    text-align: center;
-  }
 `
 
-export const EmailWrapper = styled.div`
-  
-`
+export const EmailWrapper = styled.div``
 
 export const Label = styled.p`
   color: #000000;
   font-size: 16px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   letter-spacing: 0.04em;
 `
 
@@ -90,10 +81,12 @@ export const Email = styled.input`
   border: 1px solid #000000;
   width: 264px;
   height: 40px;
-  border-radius: 10px;
+  /* border-radius: 10px; */
+  border-bottom-left-radius: 10px;
+  border-top-left-radius: 10px;
   padding-left: 20px;
   font-size: 16px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 
   @media screen and (max-width: 490px) {
     width: 85%;
@@ -105,24 +98,31 @@ export const Email = styled.input`
 export const Button = styled.p`
   background-color: #000000;
   border: 1px solid #000000;
-  color: #FFFFFF;
+  color: #ffffff;
   font-size: 16px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   padding-left: 25px;
   padding-right: 25px;
-  padding-top: 12.5px;
+  padding-top: 11.5px;
   padding-bottom: 12.5px;
-  border-radius: 10px;
+  /* border-radius: 10px; */
+  border-bottom-right-radius: 10px;
+  border-top-right-radius: 10px;
   position: relative;
   right: 20px;
   text-align: center;
   transition: 0.3s ease-in-out;
 
   :hover {
-    background-color: #FFFFFF;
+    background-color: #ffffff;
     color: #000000;
     cursor: pointer;
   }
+
+  @media screen and (max-width: 430px) {
+    padding-top: 15px;
+    padding-bottom: 12.5px;
+  }
 `
 
-export default Subscribe;
+export default Subscribe
