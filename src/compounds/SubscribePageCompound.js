6e60d3b8f8e1d18154c59efc4cd4ad999e802f44
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-02 18:14:16 +0200

    Adding the Banner that shows when clicking on the Menu icon on the mobile mode

--- src/compounds/SubscribePageCompound.js
+++ src/compounds/SubscribePageCompound.js
@@ -1,23 +1,116 @@
-import React from 'react';
+import React from "react"
 import styled from "styled-components"
-import Navbar from '../components/Subscribe/Navbar'
-import Subscribe from '../components/Subscribe/Subscribe';
+import { useHistory } from "react-router-dom"
+import Navbar from "../components/Subscribe/Navbar"
+import Subscribe from "../components/Subscribe/Subscribe"
 
 function SubscribePageCompound() {
+  const [showBanner, setShowBanner] = React.useState(false)
+
+  const toggleBanner = () => {
+    setShowBanner(!showBanner)
+  }
+
+  const history = useHistory()
+
   return (
     <Wrapper>
-      <Navbar />
+      {showBanner && (
+        <BannerWrapper>
+          <ItemsWrapper>
+            <CloseButton onClick={toggleBanner}>×</CloseButton>
+            <TechnologyOption
+              onClick={() => history.push("/TechnologyPageCompound")}
+            >
+              Technology
+            </TechnologyOption>
+            <AboutOption onClick={() => history.push("/AboutPageCompound")}>
+              About
+            </AboutOption>
+            <CareersOption onClick={() => history.push("/CareersPageCompound")}>
+              Careers
+            </CareersOption>
+            <SubscribeOption
+              onClick={() => history.push("/SubscribePageCompound")}
+            >
+              Subscribe
+            </SubscribeOption>
+          </ItemsWrapper>
+        </BannerWrapper>
+      )}
+
+      <Navbar toggleBanner={toggleBanner} />
       <Subscribe />
     </Wrapper>
-  );
+  )
 }
 
-export const Wrapper = styled.div`
-  
+export const Wrapper = styled.div``
+
+export const HeaderWrapper = styled.div``
+
+export const BannerWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  width: 100vw;
+  height: 100vh;
+  font-family: "Anton", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
 `
 
-export const HeaderWrapper = styled.div`
-  
+export const ItemsWrapper = styled.p`
+  height: 400px;
+  background-color: #ffffff;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
 `
 
-export default SubscribePageCompound;
+export const TechnologyOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const AboutOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const CareersOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #000000;
+`
+
+export const SubscribeOption = styled.p`
+  font-size: 20px;
+  border: 1px solid #ffffff;
+  color: #ffffff;
+  background-color: #000000;
+  padding: 10px 20px;
+  border-radius: 7px;
+`
+
+const CloseButton = styled.div`
+  position: absolute;
+  top: 30px;
+  right: 40px;
+  font-size: 40px;
+  color: white;
+  cursor: pointer;
+`
+
+export default SubscribePageCompound
