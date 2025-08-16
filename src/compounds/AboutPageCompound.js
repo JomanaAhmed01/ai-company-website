f4df62765ea6b1e171e24c0ef0ca7fa977b7b04d
Jomana ahmed <ojy.ahmed@outlook.com>
2025-08-16 17:15:05 +0200

    Commenting some lines of code for later

--- src/compounds/AboutPageCompound.js
+++ src/compounds/AboutPageCompound.js
@@ -63,9 +63,9 @@ export const Wrapper = styled.div``
 export const HeaderWrapper = styled.div``
 
 export const BannerWrapper = styled.div`
-  position: fixed;
+  /* position: fixed;
   top: 0;
-  left: 0;
+  left: 0; */
   z-index: 9999;
   background-color: rgba(0, 0, 0, 0.8);
   color: white;
